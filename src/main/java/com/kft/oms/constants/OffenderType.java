f1b12a6810da801091dd326f1deac6a440ebbd11
Ferido07 <feridyz@gmail.com>
2018-12-24 14:46:55 +0300

    Modified OffenceCode.

---
+++ src/main/java/com/kft/oms/constants/OffenderType.java
@@ -0,0 +1,5 @@
+package com.kft.oms.constants;
+
+public enum OffenderType {
+    PERSON, DRIVER, VEHICLE_OWNER, ORGANIZATION, ASSOCIATION //MAYBE ADD GROUP
+}
--- src/main/java/com/kft/oms/domain/OffenceCode.java
+++ src/main/java/com/kft/oms/domain/OffenceCode.java
@@ -2,51 +2,80 @@ package com.kft.oms.domain;
 
 import com.kft.crud.domain.AbstractEntity;
 import com.kft.oms.constants.OffenceRepetitionTracker;
-
+import com.kft.oms.constants.OffenderType;
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
 
 
 @Entity
 @Table(uniqueConstraints = @UniqueConstraint(columnNames = {"offenderType", "level", "penaltyAmount", "numberLabel"}))
 public class OffenceCode extends AbstractEntity{
 
+    // The next 2 are only added for convenience purposes to the user.
+    //Header like 9.3.3
+    @Column(length = 9)
+    private String sectionHeaderLabel;
+
+    //like አሽከርካሪ ረዳትን የሚያስጠይቁ that describes the section.
+    private String sectionHeader;
+
     //Driver, Association, VehicleOwner,... maybe use an enum or interface or Abstract superclass
     //Canceled: maybe add a column or foreign key to reference the entity_type from OffenderEntity
-    @NotNull
-    private String offenderType;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 30)
+    private OffenderType offenderType;
 
-    @NotNull
-    private String level;
+    @Column(nullable = false)
+    private Short level;
 
-    @NotNull
+    @Column(nullable = false)
     private Integer penaltyAmount;
 
+    //Not an int or short because numberLabel can contain 1.1, 1.2.3
+    @Column(length = 5)
     private String numberLabel;
 
-    @NotNull
+    @Column(nullable = false)
     private String description;
 
-    @NotNull
+    @Column(nullable = false)
     private boolean offenceRepetitionConsidered = true;
 
+    @Column(length = 30)
     @Enumerated(EnumType.STRING)
     private OffenceRepetitionTracker offenceRepetitionTrackedBy = OffenceRepetitionTracker.DRIVER;
 
 
-    public String getOffenderType() {
+
+
+    public String getSectionHeaderLabel() {
+        return sectionHeaderLabel;
+    }
+
+    public void setSectionHeaderLabel(String sectionHeaderLabel) {
+        this.sectionHeaderLabel = sectionHeaderLabel;
+    }
+
+    public String getSectionHeader() {
+        return sectionHeader;
+    }
+
+    public void setSectionHeader(String sectionHeader) {
+        this.sectionHeader = sectionHeader;
+    }
+
+    public OffenderType getOffenderType() {
         return offenderType;
     }
 
-    public void setOffenderType(String offenderType) {
+    public void setOffenderType(OffenderType offenderType) {
         this.offenderType = offenderType;
     }
 
-    public String getLevel() {
+    public Short getLevel() {
         return level;
     }
 
-    public void setLevel(String level) {
+    public void setLevel(Short level) {
         this.level = level;
     }
 
