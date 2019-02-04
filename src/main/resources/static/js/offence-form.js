c212c881c601d5f13e99bb59f2b1354d2fb6018e
Ferido07 <feridyz@gmail.com>
2019-02-04 16:18:18 +0300

    Added client side validation of offence-form. But not yet complete for complicated validations like those of the offence codes or plate number.

--- src/main/resources/static/js/offence-form.js
+++ src/main/resources/static/js/offence-form.js
@@ -20,6 +20,10 @@ if(typeof description === "undefined")
 var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
+var validator = $("#regForm").validate({
+    errorClass: "text-danger"
+});
+
 function showTab(n) {
     // This function will display the specified tab of the form...
     var x = document.getElementsByClassName("tab");
@@ -59,22 +63,14 @@ function nextPrev(n) {
 }
 
 function validateForm() {
-    //Todo: remove the return true and implement validation logic
-    return true;
     // This function deals with validation of the form fields
-    var x, y, i, valid = true;
-    x = document.getElementsByClassName("tab");
-    y = x[currentTab].getElementsByTagName("input");
-    // A loop that checks every input field in the current tab:
-    for (i = 0; i < y.length; i++) {
-        // If a field is empty...
-        if (y[i].value === "") {
-            // add an "invalid" class to the field:
-            y[i].className += " invalid";
-            // and set the current valid status to false
-            valid = false;
-        }
-    }
+    var tabs, inputs, valid = true;
+    tabs = $(".tab");
+    var tab = $(tabs.get(currentTab));
+    inputs = tab.find("input");
+    inputs.each(function (index, value) {
+        valid = validator.element(value) && valid;
+    });
     // If the valid status is true, mark the step as finished and valid:
     if (valid) {
         document.getElementsByClassName("step")[currentTab].className += " finish";
