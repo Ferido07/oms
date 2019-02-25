0c3b16b3f49964bca2698bbcc560b8a019561e3a
Ferido07 <feridyz@gmail.com>
2019-02-25 11:20:46 +0300

    Added a date switch feature for the display pages.

--- src/main/resources/static/js/offence-show.js
+++ src/main/resources/static/js/offence-show.js
@@ -66,3 +66,26 @@ $("span.step").on("click", function () {
     // console.log(step);
     showTab(step);
 });
+
+var dateType = 'gregorian';
+function changeDate() {
+    var ethiopianDates = $("#ethiopianDate");
+    var gregorianDates = $("#gregorianDate");
+    var dateSelectorButton = $("#dateSelector");
+    //change to ethiopian
+    if(dateType === 'gregorian'){
+        ethiopianDates.removeClass("d-none");
+        gregorianDates.addClass("d-none");
+        dateSelectorButton.text("To Gregorian");
+        dateType = 'ethiopian';
+        //console.log('change date to ...' + dateType);
+    }
+    //change to gregorian
+    else if(dateType === 'ethiopian'){
+        gregorianDates.removeClass("d-none");
+        ethiopianDates.addClass("d-none");
+        dateSelectorButton.text("To Ethiopian");
+        dateType = 'gregorian';
+        //console.log('change date to ...' + dateType);
+    }
+}
